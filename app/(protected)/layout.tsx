246c30dd3619d75f1eaa8a25a124b01044f5fe9a
deepakgusain-tech <deepak.gusain@technosyslabs.com>
2026-04-21 18:00:37 +0530

    header layout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,7 +1,11 @@
 import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/site-header";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { ChevronDown } from "lucide-react";
 
 export default function RootLayout({
     children,
@@ -12,30 +16,18 @@ export default function RootLayout({
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-                    <div className="flex items-center gap-2 px-4">
-                        <SidebarTrigger className="-ml-1" />
-                        <Separator
-                            orientation="vertical"
-                            className="mr-2 data-vertical:h-4 data-vertical:self-auto"
-                        />
-                        <Breadcrumb>
-                            <BreadcrumbList>
-                                <BreadcrumbItem className="hidden md:block">
-                                    <BreadcrumbLink href="#">
-                                        Build Your Application
-                                    </BreadcrumbLink>
-                                </BreadcrumbItem>
-                                <BreadcrumbSeparator className="hidden md:block" />
-                                <BreadcrumbItem>
-                                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                                </BreadcrumbItem>
-                            </BreadcrumbList>
-                        </Breadcrumb>
+                {/* ✅ TOP HEADER */}
+                <div className="sticky top-0 z-50 bg-white">
+                    <SiteHeader />
+                </div>
+
+                {/* ✅ MAIN CONTENT */}
+                <div className="flex flex-1 flex-col">
+                    <div className="@container/main flex flex-1 flex-col gap-2">
+                        <div className="flex flex-col gap-4 p-4 py-4 md:gap-6 md:py-6">
+                            {children}
+                        </div>
                     </div>
-                </header>
-                <div className="p-4">
-                    {children}
                 </div>
             </SidebarInset>
         </SidebarProvider>
